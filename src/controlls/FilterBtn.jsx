3b9226b6239c5c64763b053dfd4c54a7e7bb3230
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-03-02 15:08:24 +0300

    Add files via upload

--- src/controlls/FilterBtn.jsx
+++ src/controlls/FilterBtn.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Plus, Success } from '../icons/svgIcon'
-
+import React, { lazy } from 'react'
+import horror from "../icons/horror.svg"
+import comedy from "../icons/comedy.png"
+import Animition from "../icons/Animtion.svg"
+import crime from "../icons/crime.svg"
+import Adventure from '../icons/adventure.svg'
+import action from "../icons/action.svg"
 function FilterBtn(props) {
+  const icons = {
+    Horror:horror,
+    Comedy:comedy,
+    Animation:Animition,
+    Crime:crime,
+    Adventure:Adventure,
+    Action:action
+  }
   return (
-   <div  {...props} className={`flex-items box ${props.active ==="true"?"bg-main":"bg-dark"} flex flex-between`}><p>{props.name}</p>
-   {props.active === "true"?<Success width="20px"/>:<Plus/>}
-   
-   
+   <div  {...props} className={`filter-btn-box flex-items box ${props.active ==="true"?"filter-btn-active":"bg-dark"} flex flex-between f-column`}>
+   {/* {props.active === "true"?<Success width="20px"/>:<Plus/>} */}
+   <img src={icons[props.name]} alt="" srcset="" className='filter-btn-img' />
+   <p>{props.name}</p>
    </div>
   )
 }
