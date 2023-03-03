e5a1ccd3419cc88f767ae9a21c48084dca5fb0a6
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-03-03 23:52:24 +0300

    Update Feed.jsx

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -4,25 +4,33 @@ import { Link, Outlet,  } from 'react-router-dom';
 import { Apps, BookMark, Home,Notifications } from '../../icons/svgIcon';
 import './feed.css'
 const Feed =memo(({active,setActive}) => {
+  const[activeSearch,setActiveSearch]=useState(false)
   // setactive function to open and close  rightbar
   return (
     <div className="flex-box-3 bg-dark grid feed hidden-x feed-box">
         <nav className="flex flex-between feed-nav" >
 {/*============= url to the the mian pages==============================*/ }
              <div>
-                 <ul className="flex primary-navigation ">
-                   <Link to='' className="text-white "><span className='logo-tm'> 12</span>:00<span className='logo-dt'>pm</span></Link>
+                 <ul className="flex primary-navigation " style={{gap:"10px"}}>
+                  {/* <Link to='' className="text-white "><Home width={'20px'}/></Link> */}
+                  <Link to='' className="text-white "><span className='logo-tm'> 12</span>{!activeSearch&&":00"}<span className='logo-dt'>pm</span></Link>
+                  <div className={`mobile-search bg-dark center text-gray ${activeSearch&&"active-search"}`}>
+     <img className="" src={search}alt="" onClick={()=>setActiveSearch(true)} />
+     <input className={`Search-input`} type="search" name="" id="" />
+    </div>
+                 {/* <Link to='/explore/movie' state={{ dataType: 'explore' }} className="text-gray">movies</Link>
+                 <Link to='/explore/tv' state={{ dataType: 'explore' }}     className="text-gray">tv show</Link> */}
                  </ul>
              </div>
 {/*============= url to the the another pages============================*/ }
              <div>
                  <ul className="flex primary-navigation">
-                 <Link to='notification' className="svg-icon">
+                   {!activeSearch&&<> <Link to='notification' className="svg-icon">
                     <Notifications width={'20px'} />
                  </Link>
                  <Link to='fav' className="svg-icon">
                     <BookMark/>
-                 </Link>
+                 </Link></>}
                  <li id="nav-block" className="svg-icon" onClick={()=>setActive(!active)}>
                     <Apps />
                  </li>
