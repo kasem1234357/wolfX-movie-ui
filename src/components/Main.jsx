5b0712a6d17c6d7e1a69a34f33a583751b98367d
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-03-03 21:38:33 +0300

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React from 'react'
+import {Link } from 'react-router-dom'
 import { useEffect } from 'react';
 import { useState } from 'react';
 import ScrollBox from '../controlls/ScrollBox';
@@ -14,7 +15,7 @@ function Main() {
   });
   const fetchData = async () => {
     try {
-      const result = await axios(fetchUrl.trending(),);
+     const result = await axios(fetchUrl.explore("movie",1),);
       const rated = await axios(fetchUrl.rated());
       const series= await axios(fetchUrl.tvShow())
       setData({rated:rated.data.results,all:result.data.results,series:series.data.results});
@@ -33,20 +34,20 @@ function Main() {
     return <Loading/>
   }
   return (
-   <div className=" feed-list grid scroll-y padding hidden-x"  >
+    <div className=" feed-list grid scroll-y padding hidden-x"  >
    <div className='flex flex-between ' style={{paddingTop:'10px',maxWidth:'98vw'}}>
      <h3 className='text-white'>Trending Movies</h3>
-     <p className='text-gray flex center' >see All <Arrow/></p>
+     <p className='text-gray flex center' ><Link to="/explore/movie">see All</Link><Arrow/></p>
    </div>
    <ScrollBox style={{ minHeight: "270px",position:'relative'}} data={data3.all}type={'large-movie'}/>
     <div className='flex flex-between ' style={{paddingTop:'10px',maxWidth:'98vw'}}>
      <h3 className='text-white'>Top tv show </h3>
-     <p className='text-gray flex center'>see All <Arrow/></p>
+     <p className='text-gray flex center'><Link to="/explore/tv">see All</Link><Arrow/></p>
    </div>
    <ScrollBox style={{ minHeight: "270px",position:'relative'}} data={data3.series}type={'meduim-movie'} dataType={'tv'}/>
 <div className='flex flex-between '  style={{paddingTop:'10px',maxWidth:'98vw'}}>
      <h3 className='text-white'>Top rating</h3>
-     <p className='text-gray flex center'>see All <Arrow/></p>
+     <p className='text-gray flex center'><Link to="/explore/movie">see All</Link><Arrow/></p>
    </div>
    <ScrollBox style={{ minHeight: "270px",position:'relative'}} data={data3.rated}type={'small-movie'}/>
 </div>
