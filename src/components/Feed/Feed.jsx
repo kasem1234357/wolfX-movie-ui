2506c6d0a02f84ff94d394d53427280acf8b6a00
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-03-04 00:06:27 +0300

    Update Feed.jsx

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,11 +1,15 @@
 import React, { memo } from 'react'
 import search from '../../images/search-svgrepo-com.svg'
-import { Link, Outlet,  } from 'react-router-dom';
+import { Link,  Outlet, useNavigate,  } from 'react-router-dom';
 import { Apps, BookMark, Home,Notifications } from '../../icons/svgIcon';
-import { useState } from 'react';
+import {searchUpdate} from '../../utils/movies'
+import {useDispatch,useSelector}from 'react-redux'
 import './feed.css'
+import { useState } from 'react';
 const Feed =memo(({active,setActive}) => {
-  const[activeSearch,setActiveSearch]=useState(false)
+   const[activeSearch,setActiveSearch]=useState(false)
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
   // setactive function to open and close  rightbar
   return (
     <div className="flex-box-3 bg-dark grid feed hidden-x feed-box">
@@ -17,7 +21,17 @@ const Feed =memo(({active,setActive}) => {
                   <Link to='' className="text-white "><span className='logo-tm'> 12</span>{!activeSearch&&":00"}<span className='logo-dt'>pm</span></Link>
                   <div className={`mobile-search bg-dark center text-gray ${activeSearch&&"active-search"}`}>
      <img className="" src={search}alt="" onClick={()=>setActiveSearch(true)} />
-     <input className={`Search-input`} type="search" name="" id="" />
+     <input className={`Search-input`} type="search" name="" id="" onInput={(e)=>{
+       if(window.location.pathname === ('/explore/tv')){
+        navigate("explore/tv",{ state: { dataType: 'search' } })
+       }else{
+        navigate("explore/movie",{ state: { dataType: 'search'} })
+       }
+       if(e.target.value === ''){
+        navigate(window.location.pathname,{ state: { dataType: 'explore',filter:"" } })
+       }
+       dispatch(searchUpdate(e.target.value) )
+       ;}}/>
     </div>
                  {/* <Link to='/explore/movie' state={{ dataType: 'explore' }} className="text-gray">movies</Link>
                  <Link to='/explore/tv' state={{ dataType: 'explore' }}     className="text-gray">tv show</Link> */}
@@ -26,12 +40,13 @@ const Feed =memo(({active,setActive}) => {
 {/*============= url to the the another pages============================*/ }
              <div>
                  <ul className="flex primary-navigation">
-                   {!activeSearch&&<> <Link to='notification' className="svg-icon">
+                  {!activeSearch&&<> <Link to='notification' className="svg-icon">
                     <Notifications width={'20px'} />
                  </Link>
                  <Link to='fav' className="svg-icon">
                     <BookMark/>
                  </Link></>}
+                
                  <li id="nav-block" className="svg-icon" onClick={()=>setActive(!active)}>
                     <Apps />
                  </li>
