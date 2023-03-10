f184f66ee100e2c71e6535f7dae5a1881b6db761
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-03-10 21:33:22 +0300

    Update Download.jsx

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 const Download = () => {
-  const location = useLocation();
+   const location = useLocation();
+
   const server1 = (n,type,year,name)=>{
     const nameSv1 = type === "Movie"?name.split(" ").join(".")+"."+year:name.split(" ").join(".")
     return `http://dl${n}.sermovie.xyz/${type}/${year}/${nameSv1}/`
@@ -23,29 +24,73 @@ const Download = () => {
     const nameSv4 = name.split(" ").join(".")
     return `http://dl5.sermovie.xyz/${typeSv4}/${year}/${nameSv4}/`
   }
+  const server5 = (type,year,name)=>{
+    const typeSv5 = type === "Movie"?"Film":"Series"
+    const nameSv4 = name.split(" ").join(".")+"."+year
+    return `https://dl10.dlseemoon.xyz/${typeSv5}/SoftSub/${year}/${nameSv4}.480p.WEB-DL.RMT.SoftSub.Golchindl.mkv/`
+  }
   const name = location.state.name
   const type = location.state.type
   const year = location.state.year
   return (
-   <>
+    <>
     <div>
-     <h1>direct link 1</h1>
-     <a href={server2(type,year,name)} target="_blank" rel="noopener noreferrer"> url 1</a>
-
-     <h1>direct link 2</h1>
-
-     <a href={server1(6,type,year,name)} target="_blank" rel="noopener noreferrer"> url 2-1</a>
-     <a href={server1(6,type,parseInt(year)+1,name)} target="_blank" rel="noopener noreferrer"> url 2-2</a>
-
-     <h1>direct link 3</h1>
-
-     <a href={server4(type,parseInt(year)+1,name)} target="_blank" rel="noopener noreferrer"> url 3-2</a>
-     <a href={server4(type,year,name)} target="_blank" rel="noopener noreferrer"> url 3-1</a>
-     
-     <h1>direct link 4</h1>
-     <a href={server3(type,year,name)} target="_blank" rel="noopener noreferrer"> url 4</a>
-     <h1>direct link 5</h1>
-     <a href={server1(4,type,parseInt(year)+1,name)} target="_blank" rel="noopener noreferrer"> url 5</a>
+      <div className="direct-section">
+        <div className="download-btn">
+          <a href={server2(type,year,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>
+      <div className="direct-section">
+        <div className="download-btn">
+          <a href={server1(6,type,year,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>
+      <div className="direct-section">
+        <div className="download-btn">
+          <a href={server1(6,type,parseInt(year)+1,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>
+      <div className="direct-section">
+        <div className="download-btn">
+          <a href={server4(type,year,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>
+     <div className="direct-section">
+        <div className="download-btn">
+          <a href={server4(type,parseInt(year)+1,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>
+      <div className="direct-section">
+        <div className="download-btn">
+          <a href={server3(type,year,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>
+      <div className="direct-section">
+        <div className="download-btn">
+          <a href={server1(4,type,parseInt(year)+1,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>
+      {parseInt(year) < 2023?<div className="direct-section">
+        <div className="download-btn">
+          <a href={server5(type,year,name)} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+        </div>
+        <div className="q">720p</div>
+        <div className="size">--</div>
+      </div>:null}
     </div>
     
     </>
