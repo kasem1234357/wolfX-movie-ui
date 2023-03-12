a33cbbfb04643785e5574fc28363b7dcd4347a1b
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-03-12 18:22:26 +0300

    Update Download.jsx

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -7,27 +7,27 @@ const Download = () => {
 
   const server1 = (n,type,year,name)=>{
     const nameSv1 = type === "Movie"?name.split(" ").join(".")+"."+year:name.split(" ").join(".")
-    return `https://adfloz.co/st?api=5fc9f12a9187c2cf1ae84b604b2e3ba79cc8b492&url=http://dl${n}.sermovie.xyz/${type}/${year}/${nameSv1}/`
+    return `https://up4cash.com/st?api=222b9419cb010e4f5c5ed2c2c3c61ddde17f3037&url=http://dl${n}.sermovie.xyz/${type}/${year}/${nameSv1}/`
   }
   const server3 = (type,year,name)=>{
     const nameSv3 = name.split(" ").join(".")
     const typeSv3 = type === "Movie"?"Movies":"Series"
-    return `https://adfloz.co/st?api=5fc9f12a9187c2cf1ae84b604b2e3ba79cc8b492&url=http://dl4.sermovie.xyz/${typeSv3}/${year}/${nameSv3}/`
+    return `https://up4cash.com/st?api=222b9419cb010e4f5c5ed2c2c3c61ddde17f3037&url=http://dl4.sermovie.xyz/${typeSv3}/${year}/${nameSv3}/`
   }
   const server2 =(type,year,name)=>{
      const typeSv = type === "Movie"?"Movie":"TVS"
      const nameSv2 = name.split(" ").join("%20")
-     return `https://adfloz.co/st?api=5fc9f12a9187c2cf1ae84b604b2e3ba79cc8b492&url=http://s1.dlcm.xyz/Zone/${typeSv}/${typeSv !== "Movie"?nameSv2:`${year}/${nameSv2}%20${year}%20720p%20x264%20Webrip%20YIFY%20MOViE-ZONE.mp4`}`
+     return `https://up4cash.com/st?api=222b9419cb010e4f5c5ed2c2c3c61ddde17f3037&url=http://s1.dlcm.xyz/Zone/${typeSv}/${typeSv !== "Movie"?nameSv2:`${year}/${nameSv2}%20${year}%20720p%20x264%20Webrip%20YIFY%20MOViE-ZONE.mp4`}`
   }
   const server4 = (type,year,name)=>{
     const typeSv4 = type === "Movie"?"Movies":"Series"
     const nameSv4 = name.split(" ").join(".")
-    return `https://adfloz.co/st?api=5fc9f12a9187c2cf1ae84b604b2e3ba79cc8b492&url=http://dl5.sermovie.xyz/${typeSv4}/${year}/${nameSv4}/`
+    return `https://up4cash.com/st?api=222b9419cb010e4f5c5ed2c2c3c61ddde17f3037&url=http://dl5.sermovie.xyz/${typeSv4}/${year}/${nameSv4}/`
   }
   const server5 = (type,year,name)=>{
     const typeSv5 = type === "Movie"?"Film":"Series"
     const nameSv4 = name.split(" ").join(".")+"."+year
-    return `https://adfloz.co/st?api=5fc9f12a9187c2cf1ae84b604b2e3ba79cc8b492&url=https://dl10.dlseemoon.xyz/${typeSv5}/SoftSub/${year}/${nameSv4}.480p.WEB-DL.RMT.SoftSub.Golchindl.mkv/`
+    return `https://up4cash.com/st?api=222b9419cb010e4f5c5ed2c2c3c61ddde17f3037&url=https://dl10.dlseemoon.xyz/${typeSv5}/SoftSub/${year}/${nameSv4}.480p.WEB-DL.RMT.SoftSub.Golchindl.mkv/`
   }
   const name = location.state.name
   const type = location.state.type
