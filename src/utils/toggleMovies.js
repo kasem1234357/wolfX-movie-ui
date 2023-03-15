ded5f760bce28269cf8781653cb12f1570f17909
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-03-15 19:34:30 +0300

    Update toggleMovies.js

--- src/utils/toggleMovies.js
+++ src/utils/toggleMovies.js
@@ -3,7 +3,12 @@ import {addMovie, deleteMovies} from '../utils/movies'
 import axios from 'axios'
 
 export const ToggleMovies = ({user,data,dispatch,setCheck,name,movies})=>{
- const check = movies?.some(movie => movie.imdb_id === data.imdb_id)
+ let check = false
+  if(name === "movie"){
+     check = movies?.some(movie => movie.imdb_id === data.imdb_id)
+  }else{
+    check = movies?.some(movie => movie.id === data.id)
+  }
  if(check){
    try {
      axios.put(`https://wolfx-movie.onrender.com/api/users/${user._id}/deleteMovie`,{...data.all,typeShow:name}).then(() =>{
