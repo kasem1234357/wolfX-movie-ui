57a21faece31b13fd9f5ccf50d780fa7412c4446
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-08-24 17:28:23 +0300

    Delete src/src directory

--- src/src/controlls/Reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import moviesReducer from '../utils/movies'
-import usersReducer from '../utils/users'
-export default combineReducers({
- movies: moviesReducer,
- users:usersReducer
-})
