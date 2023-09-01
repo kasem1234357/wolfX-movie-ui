4dde234923e05ca5960bb453d5a3d41baff36d57
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-09-01 10:10:00 +0300

    Update toggleMovies.js

--- src/utils/toggleMovies.js
+++ src/utils/toggleMovies.js
@@ -12,7 +12,7 @@ export const ToggleMovies = ({user,data,dispatch,setCheck,name,movies})=>{
   }
  if(check){
    try {
-     axios.put(`https://wolfx-movie.onrender.com/api/users/${user._id}/deleteMovie`,{movieId : data.id}).then(() =>{
+     axios.put(`https://wolfxmovie2.onrender.com/api/users/${user._id}/deleteMovie`,{movieId : data.id}).then(() =>{
        dispatch(deleteMovies({...data,typeShow:name}))
        handleClick({type:"warning",msg:"deleting movie from faviorate"})
      })
@@ -22,7 +22,7 @@ export const ToggleMovies = ({user,data,dispatch,setCheck,name,movies})=>{
    }
  }else{
    try {
-     axios.put(`https://wolfx-movie.onrender.com/api/users/${user._id}/addMovie`,{data:{...data,typeShow:name}}).then((responce) =>{
+     axios.put(`https://wolfxmovie2.onrender.com/api/users/${user._id}/addMovie`,{data:{...data,typeShow:name}}).then((responce) =>{
        dispatch(addMovie({...data,typeShow:name}))
        handleClick({type:"success",msg:"movie added successifly"})
        setCheck(true)
