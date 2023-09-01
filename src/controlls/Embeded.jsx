957b1d9836fbd39c070a3da6c5913e541eb5158f
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-09-01 11:09:23 +0300

    Update Embeded.jsx

--- src/controlls/Embeded.jsx
+++ src/controlls/Embeded.jsx
@@ -5,7 +5,7 @@ console.log(id);
   return (
     <div className='embedBox'>
   
-     <iframe width={100} height={100} title='youtube' style={{border:"none"}} SameSite="None"  src={`https://autoembed.to/trailer/${name}/${id}`}  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+     <iframe width={100} height={100} title='youtube' style={{border:"none"}} SameSite="None" src={name ==='movie'?`https://autoembed.to/${name}/imdb/${id}?trailer=1`:`https://autoembed.to/${name}/tmdb/${id}-1-1?trailer=1`}  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       />
     </div>
