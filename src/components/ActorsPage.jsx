eafc051e77fe4c3763321907f2d2228668bf5319
kasem1234357 <kasemalolo2002@gmail.com>
2023-12-16 12:02:00 +0200

    features search by actor

--- src/components/ActorsPage.jsx
+++ src/components/ActorsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { fetchUrl } from '../controlls/FetshingUrl'
+import Loading from './Loading'
 
 function ActorsPage() {
     const location = useLocation()
