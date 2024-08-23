4d9088e86c5922c6dd600b60d842b7f19e298c70
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2024-08-23 17:51:21 +0300

    Update Download.jsx

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -3,6 +3,7 @@
 import React, {  useEffect, useState } from 'react'
 import {  useSearchParams } from 'react-router-dom'
 import Loading from '../components/custom/Loading';
+import axios from 'axios'
 import { Helmet } from 'react-helmet';
 import { getMainDownloadLink } from '../utils/getMainDownloadLink';
 const checkRange = (name) => { 
@@ -35,6 +36,12 @@ const Download = () => {
   const type = searchParams.get("type")
   const year = searchParams.get("year")
   const month = searchParams.get("month")
+  const  downloadFunc =(url)=>{
+   axios.get(`https://download-movie-url-generator.onrender.com/server1/download?url=${url}`).then(res=>{
+  console.log(res.data.url)
+})
+}
+
   useEffect(()=>{
     getMainDownloadLink(
      { name: name, year, month, range: checkRange(name) },
@@ -67,8 +74,10 @@ const Download = () => {
     <div>
       {dt.length == 0 ?<div  style={{height:'min-content',background:'#353b41',padding:'20px 10px',textAlign:'center',color:'#fff'}}><h1>no url found sorry</h1></div> :dt?.map(el =>(
        <div className="direct-section">
-       <div className="download-btn">
-         <a href={el.url} target="_blank" className="direct-link" rel="noopener noreferrer">download</a>
+       <div className="download-btn" onClick={()=>{
+           downloadFunc(el.url)
+       }}>
+         <p className="direct-link" >download</p>
        </div>
        <div className="q">{el.res}</div>
        <div className="q">{el.q || 'WEBDL'}</div>
